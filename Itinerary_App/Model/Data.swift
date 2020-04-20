71b1cea1e4f01b9d7e33d66c9fe67f2d76483db9
QThomas <qmadinah@gmail.com>
2020-04-20 17:00:03 -0400

    Added readTrips code

---
+++ Itinerary_App/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Itinerary_App
+//
+//  Created by Qadriyyah Griffin on 4/20/20.
+//  Copyright © 2020 Qadriyyah Thomas. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+  static var tripModels = [TripModel]()
+  
+}
